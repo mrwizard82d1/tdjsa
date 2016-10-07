cde77b83e11301991bb6f8647b8290cbf46a292b
Larry Jones <larryj@blinds.com>
2016-10-07 10:28:18 -0500

    First passing palindrome test: "mom"

--- tdd/server/palindrome/test/palindrome-test.js
+++ tdd/server/palindrome/test/palindrome-test.js
@@ -1,7 +1,12 @@
 var expect = require('chai').expect;
+var isPalindrome = require('../src/palindrome');
 
 describe('palindrome-test', function() {
 	it('should pass the canary test', function() {
 		expect(true).to.be.true;
+	});
+
+	it('should return true when passed "mom"', function() {
+		expect(isPalindrome('mom')).to.be.true;
 	});
 });
