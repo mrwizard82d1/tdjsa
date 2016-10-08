564353780fbff7ccc18f7365b4c811b34fc890f1
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-10-08 18:56:29 -0500

    Additional tests:
    * 'mom mom' is a palindrome
    * 'dad dae' is not a palindrome
    * '' is not a palindrome
    * '  ' is not a palindrome

--- tdd/client/palindrome/src/palindrome.js
+++ tdd/client/palindrome/src/palindrome.js
@@ -1,3 +1,8 @@
 var isPalindrome = function(phrase) {
+	// An empty phrase is **not** a palindrome
+	if (phrase.length == 0) {
+		return false;
+	}
+
 	return phrase.split('').reverse().join('') === phrase;
 };
--- tdd/client/palindrome/test/palindrome-test.js
+++ tdd/client/palindrome/test/palindrome-test.js
@@ -14,4 +14,20 @@ describe('palindrome-client-tests', function(){
 	it('should return false for "dude"', function() {
 		expect(isPalindrome('dude')).to.be.false;
 	});
+
+	it('should return true for "mom mom"', function() {
+		expect(isPalindrome('mom mom')).to.be.true;
+	});
+
+	it('should return false for "dad dae"', function() {
+		expect(isPalindrome('dad dae')).to.be.false;
+	});
+
+	it('should return false for ""', function() {
+		expect(isPalindrome('')).to.be.false;
+	});
+
+	it('should return false for "  "', function() {
+		expect(isPalindrome('')).to.be.false;
+	});
 });
