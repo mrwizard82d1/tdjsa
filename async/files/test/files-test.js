3f04058d5d26bc016ef67d76a565740bb7524ee0
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-10-09 18:07:47 -0500

    First "attempt" to test a function with a callback
    Executing this test "passes"; however, it verifies that the number of
    lines in an existing file is **negative**. (Not exactly a comforting
    result. :) )
    
    This "lying" test will be corrected in the next commit.

---
+++ async/files/test/files-test.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+var linesCount = require('../src/files');
+
+describe('test server-side callback', function() {
+	it('should return correct line count for a valid file', function() {
+		// A nice try, but this implementation will not actually work.
+		// The test "works," but since it expects a **negative count**
+		// (not exactly plausible).
+		var callback = function(count) {
+			expect(count).to.be.eql(-2319);
+		};
+
+		linesCount('src/files.js', callback);
+	});
+});
