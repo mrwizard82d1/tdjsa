ac25e4a5a9e4a4601446132335a0490ed9043ad1
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-10-09 19:01:48 -0500

    Test verifying async behavior on error
    Verify behavior of async function reading file when test supplies an
    invalid file name.

--- async/files/test/files-test.js
+++ async/files/test/files-test.js
@@ -3,14 +3,21 @@ var linesCount = require('../src/files');
 
 describe('test server-side callback', function() {
 	it('should return correct line count for a valid file', function(done) {
-		// A nice try, but this implementation will not actually work.
-		// The test "works," but since it expects a **negative count**
-		// (not exactly plausible).
 		var callback = function(count) {
 			expect(count).to.be.eql(15);
 		};
 
 		linesCount('src/files.js', callback);
-		// done();
+		done();
+	});
+
+	it('should report an error for an invalid file name', function(done) {
+		var onError = function(error) {
+			expect(error).to.be.eql('unable to open file src/files.js');
+			done();
+		};
+
+		linesCount('src/files.js', function() { return undefined; }, onError);
+		done();
 	});
 });
