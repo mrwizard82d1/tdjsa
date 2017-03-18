41ca2f259ef5f0985d496dd72175f03b8c13d2ef
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2017-03-18 08:46:50 -0500

    And now it passes.

--- tdd/sample/test/util_test.js
+++ tdd/sample/test/util_test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect
 
 describe('util tests', function() {
 	it('should pass the canary test', function() {
-		expect(true).to.eql(false);
+		expect(true).to.eql(true);
 	});
 });
 
