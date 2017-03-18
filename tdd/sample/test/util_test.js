cb8f8fd25520461bd1df635bc2a370e3904ac5f6
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2017-03-18 08:51:34 -0500

    Failing test to convert degrees F to degrees C
    Currently, this test fails. I have created `src/utils.js`; however, the
    test reports that:
    
        TypeError: Util is not a constructor

--- tdd/sample/test/util_test.js
+++ tdd/sample/test/util_test.js
@@ -1,8 +1,22 @@
-var expect = require('chai').expect
+var expect = require('chai').expect;
+var Util = require('../src/util');
 
 describe('util tests', function() {
 	it('should pass the canary test', function() {
 		expect(true).to.eql(true);
+	});
+
+	var util
+	beforeEach(function() {
+		util = new Util();
+	});
+
+	it('should return 0C for 32F', function() {
+		var temperatureF = 32;
+
+		var actualC = util.f2c(temperatureF);
+
+		expect(actualC).to.eql(0);
 	});
 });
 
