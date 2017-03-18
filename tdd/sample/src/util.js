c81485bbf5eeaed0ec8b71fd12276817fbd8fcdb
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2017-03-18 09:53:04 -0500

    Refactor argument validation into function

--- tdd/sample/src/util.js
+++ tdd/sample/src/util.js
@@ -2,11 +2,16 @@ var isNumeric = function(toTest) {
 	return (typeof toTest === 'number' && ! isNaN(toTest));
 };
 
+var validateFahrenheit = function(temperatureF) {
+	if (! isNumeric(temperatureF)) {
+		throw new TypeError('Argument, <' + temperatureF + '>, is not a number.');
+	};
+};
+
 module.exports = function() {
 	this.f2c = function(temperatureF) {
-		if (! isNumeric(temperatureF)) {
-			throw new TypeError('Argument, <' + temperatureF + '>, is not a number.');
-		};
+		validateFahrenheit(temperatureF);
+
 		return (temperatureF - 32) * 5 / 9;
 	};
 }
