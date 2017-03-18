d1873d0f19b5c3fca65d300fa9f41d4c37690c76
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2017-03-18 14:50:26 -0500

    Addition palindrome tests:
    * "maam" is a palindrom
    * "mom mom" is
    * "mom mon" is not
    * "" is
    * "  " is

--- tdd/server/palindrome/test/palindrome-test.js
+++ tdd/server/palindrome/test/palindrome-test.js
@@ -3,6 +3,7 @@ var isPalindrome = require("../src/palindrome");
 
 /*
  * Test ideas
+ * 'maam' is a palindrome
  * 'mom mom' is a palindrome
  * 'mom mon' is not a palindrome
  * '' is not a palindrome
@@ -39,6 +40,46 @@ describe("palindrome", function() {
 		actualResult = isPalindrome(toTest);
 
 		expect(actualResult).to.be.false;
+	});
+
+	it("should return true for 'maam'", function() {
+		var toTest = "maam";
+
+		actualResult = isPalindrome(toTest);
+
+		expect(actualResult).to.be.true;
+	});
+
+	it("should return true for 'mom mom'", function() {
+		var toTest = "mom mom";
+
+		actualResult = isPalindrome(toTest);
+
+		expect(actualResult).to.be.true;
+	});
+
+	it("should return false for 'mom mon'", function() {
+		var toTest = "mom mon";
+
+		actualResult = isPalindrome(toTest);
+
+		expect(actualResult).to.be.false;
+	});
+
+	it("should return true for ''", function() {
+		var toTest = "";
+
+		actualResult = isPalindrome(toTest);
+
+		expect(actualResult).to.be.true;
+	});
+
+	it("should return true for '  '", function() {
+		var toTest = "  ";
+
+		actualResult = isPalindrome(toTest);
+
+		expect(actualResult).to.be.true;
 	});
 
 });
