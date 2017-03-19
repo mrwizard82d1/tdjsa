a4bd62f69e454ad67022126ba96fa9db497522ae
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2017-03-18 22:20:17 -0500

    Extract code into methods named for intent

--- tdd/server/palindrome/src/palindrome.js
+++ tdd/server/palindrome/src/palindrome.js
@@ -1,22 +1,33 @@
 module.exports = function(phraseToTest) {
-	if (arguments.length < 1) {
-		throw TypeError('Argument required');
-	}
-	else if (arguments.length > 1) {
-		throw TypeError('Exactly one argument required')
-	}
-	else if (typeof phraseToTest !== "string") {
-		throw TypeError('String argument required')
-	}
+	var validateArguments = function(argsToValidate) {
+		if (argsToValidate.length < 1) {
+			throw TypeError('Argument required');
+		}
+		else if (argsToValidate.length > 1) {
+			throw TypeError('Exactly one argument required')
+		}
+		else if (typeof argsToValidate[0] !== "string") {
+			throw TypeError('String argument required')
+		}
+	};
 
-	if (!phraseToTest || !phraseToTest.trim()) {
+	var hasContent = function(phraseToTest) {
+		return phraseToTest && phraseToTest.trim()
+	};
+
+	var hasPunctuation = function(phraseToTest) {
+		return phraseToTest.match(/[^A-Za-z\s]/);
+	};
+
+	validateArguments(arguments);
+
+	if (!hasContent(phraseToTest)) {
 		return false;
 	}
-	else if (phraseToTest.match(/[^A-Za-z\s]/)) {
+	else if (hasPunctuation(phraseToTest)) {
 		return false;
 	}
 
-	console.log(`phraseToTest=${phraseToTest}`);
 	var reversedWordToTest = phraseToTest.trim().split('').reverse().join('');
 	return phraseToTest.trim() == reversedWordToTest;
 };
