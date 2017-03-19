3f69946ef2770a6acc7a5e2bc4ed4ebf6dc62bb6
Larry Jones <larryj@blinds.com>
2017-03-19 18:31:31 -0500

    Write `isPalindrome`
    * Throw exception if no arguments
    * Throw exception if more than one argument
    * Throw exception if non-string argument

--- tdd/client/palindrome/src/palindrome.js
+++ tdd/client/palindrome/src/palindrome.js
@@ -3,7 +3,29 @@ var isPalindrome = function(phraseToTest) {
 		return phraseToTest && phraseToTest.trim();
 	};
 
+	var hasPunctuation = function(phraseToTest) {
+		return phraseToTest.match(/[^A-Za-z\s]/);
+	};
+
+	var validateArguments = function(toValidate) {
+		if (toValidate.length == 0) {
+			throw TypeError('Phrase required');
+		}
+
+		if (toValidate.length > 1) {
+			throw TypeError('Only one phrase accepted');
+		}
+
+		if (typeof toValidate[0] !== "string") {
+			throw TypeError('Only strings accepted')
+		}
+	};
+
+	validateArguments(arguments);
+
 	if (! hasContent(phraseToTest)) {
+		return false;
+	} else if (hasPunctuation(phraseToTest)) {
 		return false;
 	}
 
--- tdd/client/palindrome/test/palindrome-test.js
+++ tdd/client/palindrome/test/palindrome-test.js
@@ -59,5 +59,37 @@ describe("palindrome", function() {
 		expect(actualResult).to.be.false;
 	});
 
+	it("should return false for 'ma'am'", function() {
+		var toTest = "ma'am";
+
+		var actualResult = isPalindrome(toTest);
+
+		expect(actualResult).to.be.false;
+	});
+
+	it("should throw `TypeError` if passed no arguments", function() {
+		var wrapper = function() {
+			isPalindrome();
+		};
+
+		expect(wrapper).to.throw(TypeError, /Phrase required/);
+	});
+
+	it("should throw `TypeError` if passed more than one argument", function() {
+		var wrapper = function() {
+			isPalindrome("mom", "dude");
+		};
+
+		expect(wrapper).to.throw(TypeError, /Only one phrase accepted/);
+	});
+
+	it("should throw `TypeError` if passed non-string argument", function() {
+		var wrapper = function() {
+			isPalindrome(3.14159);
+		};
+
+		expect(wrapper).to.throw(TypeError, /Only strings accepted/);
+	});
+
 });
 
