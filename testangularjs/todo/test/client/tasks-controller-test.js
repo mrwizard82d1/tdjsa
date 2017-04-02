7ac85a1e93e22f491e8813cdd3fa1dc07fefb751
Larry Jones <larryj@blinds.com>
2017-04-01 20:48:18 -0500

    Passing canary test for client

---
+++ testangularjs/todo/test/client/tasks-controller-test.js
@@ -0,0 +1,6 @@
+describe('tasks controller tests', function() {
+	// Write canary test only if this is the first test for the front end in this project.
+	it('should pass this canary test', function() {
+		expect(true).to.be.true;
+	})
+})
